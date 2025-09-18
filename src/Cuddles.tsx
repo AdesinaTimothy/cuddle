38419898957692008088a2a670996a7c72e27a15
Your Full Name <timobest121@gmail.com>
2025-09-18 12:52:43 +0100

    edhdh

--- src/Cuddles.tsx
+++ src/Cuddles.tsx
@@ -1,7 +1,7 @@
 const Cuddles = () => {
   return (
     <div className="bg-black p-8">
-      <h1 className="text-5xl text-red-600 mb-5">Hi Suwes ❤️😍</h1>
+      <h1 className="text-4xl text-red-600 mb-5">Hi Suwes ❤️😍</h1>
       <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
       <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
       <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
