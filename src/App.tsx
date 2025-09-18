8ffd2dc725af7e98067ac4b5268e61d95821c60a
Your Full Name <timobest121@gmail.com>
2025-09-18 12:09:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Cuddle from "./cuddle";
+
+function App() {
+  return <Cuddle />;
+}
+
+export default App;
---
+++ src/Cuddle.tsx
@@ -0,0 +1,71 @@
+const Cuddle = () => {
+  return (
+    <div className="bg-black p-8">
+      <h1 className="text-6xl text-white text-red-500 mb-5">Hi Suwes ❤️😍</h1>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
+      <p className="text-xl text-white">And feed you plantain too 🥗🤩</p>
+    </div>
+  );
+};
+
+export default Cuddle;
