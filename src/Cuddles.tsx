e35be03bdda943c0b0538ff44138d04305cb1b34
Your Full Name <timobest121@gmail.com>
2025-11-09 11:34:30 +0000

    adding

--- src/Cuddles.tsx
+++ src/Cuddles.tsx
@@ -1,69 +1,53 @@
 const Cuddles = () => {
   return (
-    <div className="bg-black p-8">
-      <h1 className="text-4xl text-red-600 mb-5">Hi Suwes ❤️😍</h1>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">I want to cuddle you so bad 🫂🥹!!!!</p>
-      <p className="text-xl text-white">And feed you plantain too 🥗🤩</p>
+    <div className="bg-black min-h-screen p-8">
+      <h1 className="text-4xl text-red-600 mb-5">Hi Adejoke ❤️🥹😓</h1>
+
+      <div className="bg-black text-white w-full border flex flex-col gap-4 border-red-6-- rounded-lg px-3 py-2">
+        <p>This is me honestly apologizing once again</p>
+
+        <p>
+          I was reminising on the beautiful moments that we have shared together
+          ☹️, I can't but see how much we love each other. Honestly, It breaks
+          my heart to hear that statement of yours "You've always hurt me", I
+          had to delete that message from my chat because I was going back to it
+          and it wasn't healthy for me nor both of us.
+        </p>
+        <p>
+          I know I have hurt you, but a part of me still find it so hard to
+          believe that I have always hurt you every time, I want to believe you
+          made that statement out of anger and hurt. Sincerely , I am deeply
+          hurt and pained too, but this message is not about my feelings but
+          yours.
+        </p>
+
+        <p>
+          Back to the reason for this message. I am Sincerely sorry for hurting
+          you, I know I have said it several times and I have always meant it,
+          but this time around, I am saying that
+          <span className="text-red-700 font-bold px-2">
+            "I AM DEEPLY SORRY"
+          </span>
+          with the whole of my being.
+        </p>
+
+        <p>
+          I really do like you. The last thing I’d ever want to do is hurt you,
+          even by accident. I have realised my mistakes and at the same time
+          learnt my lessons.
+        </p>
+
+        <div>
+          <p>I AM SORRY </p>
+          <p>I AM SORRY </p>
+          <p>I AM SORRY </p>
+          <p>I AM SORRY </p>
+        </div>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+      </div>
     </div>
   );
 };
